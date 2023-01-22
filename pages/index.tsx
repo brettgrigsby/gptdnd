88f7b418c48991e63191e7b9c47bc9435197a3f4
brettgrigsby <gbrett@gmail.com>
2023-01-21 22:26:28 -0700

    style root page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
-import { Box, Button, Heading, Text } from "@chakra-ui/react"
+import {
+  Box,
+  Button,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react"
 import { useRouter } from "next/router"
+import { FormEventHandler, useState } from "react"
 
 function generateRandom4CharacterCode() {
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -12,21 +21,53 @@ function generateRandom4CharacterCode() {
 
 export default function Home() {
   const router = useRouter()
+  const [code, setCode] = useState("")
 
   const handleStartSession = () => {
-    const code = generateRandom4CharacterCode()
+    const newCode = generateRandom4CharacterCode()
+    router.push(`/rooms/${newCode}`)
+  }
+
+  const handleJoinSession: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
     router.push(`/rooms/${code}`)
   }
 
   return (
-    <Box>
-      <Heading>GPT&D&D</Heading>
-      <Text>Welcome to endless roleplaying</Text>
-      <Text>
-        Click the button below to enter a room. Share the room code with others
-        to play together
+    <Box
+      margin="auto"
+      maxWidth="720px"
+      height="100vh"
+      overflow="auto"
+      position="relative"
+      p={[2, null, null, 0]}
+      textAlign="center"
+    >
+      <Heading textAlign="center" mb={4}>
+        GPTnDnD
+      </Heading>
+      <Text mb={1}>Welcome to endless roleplaying!</Text>
+      <Text mb={4}>
+        Enter a room code to join an existing session or start a new one.
       </Text>
-      <Button onClick={handleStartSession}>Start Playing</Button>
+      <form onSubmit={handleJoinSession}>
+        <InputGroup w="250px" mx="auto">
+          <Input
+            placeholder="Enter a room code"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+          />
+          <InputRightElement w="fit-content" p={1}>
+            <Button size="sm" type="submit">
+              Join
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </form>
+      <Text my={4}>or</Text>
+      <Button w="250px" onClick={handleStartSession}>
+        Start a New Session
+      </Button>
     </Box>
   )
 }
